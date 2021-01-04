00a88a8eb2ff8f53176d2640c90fd639ab9a7fe4
Sebastien <sebastienbn8@gmail.com>
2021-01-04 15:23:06 -0500

    improved projects section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 export default function ProjectCard(props) {
-
+    const render = props.link !== '';
     return (
-        <div className="w3-col l3 m6 w3-margin-bottom">
+        <div className="w3-col w3-margin-bottom">
             <div className="w3-card">
                 <img src={props.img} alt="project" style={{ width: '100%', height: "80%" }} />
                 <div className="w3-container">
                     <h3>{props.title}</h3>
                     <p className="w3-opacity">{props.subTitle}</p>
                     <p>{props.info}</p>
-                    <p><button className="w3-button w3-light-grey w3-block"> Learn more</button></p>
+                    <p>{render ?
+                        <a href={props.link} className="w3-button w3-light-grey w3-block">See Github</a> : ''}
+                    </p>
                 </div>
             </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,59 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
+import Masonry from 'react-masonry-css';
 import MyPanda from '../images/mypanda.png';
 import BoneAppleTea from '../images/boneappletea.gif';
 import Vault from '../images/vault.jpg';
 import Game from '../images/2048.png';
+import Simulator from '../images/simulator.png';
 
 export default function Projects() {
 
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 2,
+        500: 1
+    };
     return (
         <div className="w3-container" style={{ padding: '28px 16px' }} id="projects">
             <h3 className="w3-center">PROJECTS</h3>
             <div className="w3-row-padding" style={{ marginTop: '64px' }}>
-                <ProjectCard
-                    title='MyPanda.space - Instagram clone'
-                    subTitle='Frontend: React, Backend: Python (Flask) - MongoDB'
-                    info='In a team of 5, we developed a web application to share, like and comment pictures.'
-                    img={MyPanda} />
-                <ProjectCard
-                    title='Vault - ConUHacks V - Winner 2nd place, Sponsor challenge CGI'
-                    subTitle='Frontend: Vue.js, Backend: Node.js - MySQL'
-                    info='Vault allows companies to manage and create volunteer events as well as gives volunteers the opportunity to find events easily and participate in them. Additionally, as a volunteer or event manager, you can see statistics such as your performance in past volunteering events.'
-                    img={Vault} />
-                <ProjectCard
-                    title='Bone Apple Tea'
-                    subTitle='Frontend: React Native, Backend: Python (Flask)'
-                    info='IOS application hat allows the user to easily choose recipes. Simply swipe right to like a recipe or swipe left to reject one. Instructions and Ingredients of the recipe are availabel upon clicking on the card '
-                    img={BoneAppleTea} />
-                <ProjectCard
-                    title='2048 AIs'
-                    subTitle='Python (Pygame, NEAT-Python)'
-                    info='I made two 2048 clone where one implements a Genetic algorithm and the other implements an Alpha-beta pruning algorithm to find the best possible move. Using the algorithm, the AI is able to win more than 60% of the games.'
-                    img={Game} />
+                <Masonry
+                    breakpointCols={breakpointColumnsObj}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column">
+                    <ProjectCard
+                        title='Smart-Home-Simulator'
+                        subTitle='JavaFX, MySQL'
+                        info='I lead a team of 5 students in creating a desktop Java application that allows users to simulate the environment of a smart home. Users can open/close doors, windows and lights, place individuals in various rooms, simulate a security scenario and simulate a heating scenario. Design of the house is fully customizable through a json file.'
+                        img={Simulator}
+                        link='https://github.com/sebastien-blain/Smart-Home-Simulator' />
+                    <ProjectCard
+                        title='MyPanda.space - Instagram clone'
+                        subTitle='Frontend: React, Backend: Python (Flask) - MongoDB'
+                        info='In a team of 5, we developed a web application to share, like and comment pictures.'
+                        img={MyPanda}
+                        link='https://github.com/sebastien-blain/SOEN341-Instagram' />
+                    <ProjectCard
+                        title='Vault - ConUHacks V - Winner 2nd place, Sponsor challenge CGI'
+                        subTitle='Frontend: Vue.js, Backend: Node.js - MySQL'
+                        info='Vault allows companies to manage and create volunteer events as well as gives volunteers the opportunity to find events easily and participate in them. Additionally, as a volunteer or event manager, you can see statistics such as your performance in past volunteering events.'
+                        img={Vault}
+                        link='' />
+                    <ProjectCard
+                        title='Bone Apple Tea'
+                        subTitle='Frontend: React Native, Backend: Python (Flask)'
+                        info='IOS application hat allows the user to easily choose recipes. Simply swipe right to like a recipe or swipe left to reject one. Instructions and Ingredients of the recipe are availabel upon clicking on the card '
+                        img={BoneAppleTea}
+                        link='https://github.com/sebastien-blain/BoneAppleTea' />
+                    <ProjectCard
+                        title='2048 AIs'
+                        subTitle='Python (Pygame, NEAT-Python)'
+                        info='I made two 2048 clone where one implements a Genetic algorithm and the other implements an Alpha-beta pruning algorithm to find the best possible move. Using the algorithm, the AI is able to win more than 60% of the games.'
+                        img={Game}
+                        link='' />
+                </Masonry>
             </div>
         </div>
 
