28ef31a3146234f0d07e36f6f825cb9f58b4b8da
Sebastien <sebastienbn8@gmail.com>
2022-01-02 15:46:24 -0500

    added thumbnail image

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,10 @@ const Home: NextPage = () => {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-512x512.png" />
         <meta name="theme-color" content="#222" />
+        <meta property="og:title" content="Sebastien's portfolio" />
+        <meta property="og:image" content="https://sebastien-blain.github.io/_next/static/media/qr.b6c43931.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://sebastien-blain.github.io/" />
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
