a54db0a8442722b52b224f0deaaf285b3044a7c5
Sebastien <sebastienbn8@gmail.com>
2022-01-02 15:36:59 -0500

    Deploy gh-pages

--- _next/static/GMl9dz1oG2o2hQNW8COxo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/557-d6d45815457a17d7.js","static/chunks/pages/index-4f3d756b10d850a4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
