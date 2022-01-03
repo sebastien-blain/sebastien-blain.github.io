bc61041a1e3496a640db8ef66da636ecf2b3e49d
Sebastien <sebastienbn8@gmail.com>
2022-01-03 11:18:22 -0500

    Added google verification tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ const Home: NextPage = () => {
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <meta name="google-site-verification" content="Wtp80B159BKSHhYh3ZelZoWxeKb43rozVlPeEqyhqGw" />
       </Head>
 
       <main>
