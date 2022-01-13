efa07a5387bf092195bacd57fc184b3172aa36b3
Sebastien <sebastienbn8@gmail.com>
2022-01-13 16:38:34 -0500

    Removed cell and email

--- components/contact.tsx
+++ components/contact.tsx
@@ -9,8 +9,6 @@ export default function Contact() {
                 <a style={{ textDecoration: 'none' }} className="contactIcon w3-large" href={'./CV_Sebastien_Blain-Nadeau_en_fr_2021.pdf'} download ><p className="w3-center"><i className="fa fa-file-pdf-o fa-fw w3-xxlarge w3-margin-right"/> Resume</ p></a>
                 <a style={{ textDecoration: 'none' }} className="contactIcon w3-large" href="https://github.com/sebastien-blain" target="_blank" rel="noopener noreferrer"><p className="w3-center"><i className="fa fa-github fa-fw w3-xxlarge w3-margin-right"/> Github</ p></a>
                 <a style={{ textDecoration: 'none' }} className="contactIcon w3-large" href="https://linkedin.com/in/sébastienblain-nadeau" target="_blank" rel="noopener noreferrer"><p className="w3-center"><i className="fa fa-linkedin fa-fw w3-xxlarge w3-margin-right"/> LinkedIn</ p></a>
-                <a style={{ textDecoration: 'none' }} className="contactIcon w3-large" href="tel:438-405-1092"><p className="w3-center"><i className="fa fa-phone fa-fw w3-xxlarge w3-margin-right"/> 438-405-1092</ p></a>
-                <a style={{ textDecoration: 'none' }} className="contactIcon w3-large" href="mailto: sebastienbn8@gmail.com"><p className="w3-center"><i className="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"/> sebastienbn8@gmail.com</ p></a>
                 <div className="w3-center">
                     <img src={QR.src} alt={'qr code'} width={320} height={320}/>
                 </div>
