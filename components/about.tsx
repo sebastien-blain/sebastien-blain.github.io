e6859abd438e73b112a68ff19cfec55740d363ab
Sebastien Blain-Nadeau <sebastienbn8@gmail.com>
2024-10-05 22:09:35 -0400

    Fix escaped char

--- components/about.tsx
+++ components/about.tsx
@@ -17,7 +17,7 @@ export default function About() {
                 <div className="w3-col m6" style={{padding: "0 3vw"}}>
                     <p>I am currently a Software Development Engineer II at Amazon Ads.</p>
                     <p>Over the past two years, I have developed scalable backend systems for the Ads Media Planning team, utilizing big data and machine learning to enhance global advertising solutions.</p>
-                    <p>I hold a Bachelor's degree in Software Engineering with Great Distinction from Concordia University, where I gained a strong foundation in software engineering principles and design methodologies.</p>
+                    <p>I hold a Bachelor&apos;s degree in Software Engineering with Great Distinction from Concordia University, where I gained a strong foundation in software engineering principles and design methodologies.</p>
                     <p>During my internships at <a href="https://www.conova.ai/" target="_blank" rel="noopener noreferrer"><b>Conova</b></a>, <a href="https://www.genetec.com/" target="_blank" rel="noopener noreferrer"><b>Genetec</b></a>, and <a href="https://accedian.com/" target="_blank" rel="noopener noreferrer"><b>Accedian</b></a>, I honed my skills in full-stack development, API design, and system integration, preparing me to tackle complex technical challenges in a fast-paced environment.</p>
                     </div>
             </div>
